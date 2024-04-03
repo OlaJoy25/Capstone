482305ea2891db4d591edcb378dfa3b091ad13fc
OlaJoy25 <olajoycelyn.ak@gmail.com>
2024-04-03 11:31:02 +0200

    final

--- src/components/pages/experts.jsx
+++ src/components/pages/experts.jsx
@@ -15,18 +15,19 @@ export const Experts = () => {
                         </a>
                         <div class="p-5">
                             <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="https://www.linkedin.com/in/joycelyn-ajike-407b85204">Joycelyn Ola Ajike</a>
+                                <a >Joycelyn Ola Ajike</a>
                             </h3>
                             <span class="text-gray-500 dark:text-gray-400">Founder & Cybersecurity Analyst</span>
                             <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Joycelyn has the rare capacity to convert difficult technical ideas into workable plans, enabling our clients to make wise choices to successfully reduce risks.</p>
                             <ul class="flex space-x-4 sm:mt-0">
                                 <li>
-
+                                <a href="https://www.linkedin.com/in/joycelyn-ajike-407b85204">
                                     <svg
                                         className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
                                     > 
                                         <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
                                     </svg>
+                                    </a>
                                 </li>
                             </ul>
                         </div>
@@ -37,17 +38,19 @@ export const Experts = () => {
                         </a>
                         <div class="p-5">
                             <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="https://www.linkedin.com/in/olayinka-naa-dzama-wilson-kofi-08a4aa7/">Olayinka Naa Dzama Wilson-Kofi</a>
+                                <a>Olayinka Naa Dzama Wilson-Kofi</a>
                             </h3>
                             <span class="text-gray-500 dark:text-gray-400">Information Security & Privacy Specialist for Ericsson(Middle East & Africa)</span>
                             <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Olayinka has been in the information security and privacy field for more than ten years. She is skilled at navigating the complex world of cybersecurity,and making sure our clients' data is safe and their privacy is respected.</p>
                             <ul class="flex space-x-4 sm:mt-0">
                                 <li>
+                                <a href="https://www.linkedin.com/in/olayinka-naa-dzama-wilson-kofi-08a4aa7/">
                                     <svg
                                         className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
                                     </svg>
+                                 </a>
                                 </li>
                             </ul>
                         </div>
@@ -58,17 +61,19 @@ export const Experts = () => {
                         </a>
                         <div class="p-5">
                             <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="https://www.linkedin.com/in/sunday-azeez-695042188/">Azeez Sunday Arisekola</a>
+                                <a>Azeez Sunday Arisekola</a>
                             </h3>
                             <span class="text-gray-500 dark:text-gray-400">GRC officer</span>
                             <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Sunday is our trusted officer. He has a solid background in Governance, Risk, and Compliance (GRC) and carefully aligns our cybersecurity tactics with industry standards and regulatory regulations, strengthening our clients' risk management frameworks with accuracy and foresight.</p>
                             <ul class="flex space-x-4 sm:mt-0">
                                 <li>
+                                <a href="https://www.linkedin.com/in/sunday-azeez-695042188/">
                                     <svg
                                         className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
                                     </svg>
+                                 </a>
                                 </li>
                             </ul>
                         </div>
@@ -79,22 +84,25 @@ export const Experts = () => {
                         </a>
                         <div class="p-5">
                             <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="https://www.linkedin.com/in/faiza-seidu-adam-591261132/">Faiza Seidu-Adam</a>
+                                <a>Faiza Seidu-Adam</a>
                             </h3>
                             <span class="text-gray-500 dark:text-gray-400">Comptia Security+ & Soc Analyst</span>
                             <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Faiza contributes a lot of experience in quickly identifying, evaluating, and handling security issues as a certified CompTIA Security+ professional and a specialist in Security Operations Center (SOC) administration, strengthening our clients' defenses against cyberattacks.</p>
                             <ul class="flex space-x-4 sm:mt-0">
                                 <li>
+                                <a href="https://www.linkedin.com/in/faiza-seidu-adam-591261132/">
                                     <svg
                                         className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
                                     </svg>
+                                 </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                </div>
+                    
+                </div> 
             </div>
         </section>
     </div>;
--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
             <ul className="flex md:order-1 md:mb-0">
               <li>
                 <Link
-                  to="/"
+                  to="https://www.linkedin.com/in/haven-cybersecurity-consult-259724205"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="Twitter"
                 >
@@ -29,7 +29,7 @@ export default function Footer() {
               </li>
               <li className="ml-4">
                 <Link
-                  to="/"
+                  to="https://www.linkedin.com/in/haven-cybersecurity-consult-259724205"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="Instagram"
                 >
@@ -46,7 +46,7 @@ export default function Footer() {
               </li>
               <li className="ml-4">
                 <Link
-                  to="/"
+                  to="https://www.linkedin.com/in/haven-cybersecurity-consult-259724205"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="Linkedin"
                 >
@@ -56,7 +56,7 @@ export default function Footer() {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
-                  </svg>
+                  </svg>  
                 </Link>
               </li>
             </ul>
